43a4d0b2bb4b504673483292a7513cb74f0f543b
Paradidle277 <145032436+Paradidle277@users.noreply.github.com>
2023-10-13 10:46:20 +0100

    Animation code proken on player controller, added sprite sheets

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public GameObject groundChecker;
     public LayerMask whatIsGround;
     public float jumpForce;
+    Animator anim;
 
     float maxSpeed = 8.0f;
     bool isOnGround = false;
@@ -20,8 +21,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() 
-        {
+    void Update()
+    {
         //create a 'float' that will be equal to the players input
         //float movementValueX = Input.GetAxis("Horizontal");
         float movementValueX = 1.0f;
@@ -29,31 +30,42 @@ public class PlayerController : MonoBehaviour
         //Change the Xvelocity of the rigidBody2D to be equal to the movement value
         playerObject.velocity = new Vector2(movementValueX * maxSpeed, playerObject.velocity.y);
 
-            //Check to see if the ground check object is touching the ground
-            isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+        //Check to see if the ground check object is touching the ground
+        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
 
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    maxSpeed = 10.0f;
-                }
-                else
-                {
-                    maxSpeed = 5.0f;
-                }
+                maxSpeed = 10.0f;
+            }
+            else
+            {
+                maxSpeed = 5.0f;
+            }
 
             //create a 'float' tht will be equal to the players horizontal input
             //float movementValueX = Input.GetAxis("Horizontal");
 
             //Set movementValueX to 1.0f, so that we always run foreward and no longer care about player input
 
-                if ((isOnGround == true) && (Input.GetAxis("Jump") > 0.0f))
+            if ((isOnGround == true) && (Input.GetAxis("Jump") > 0.0f))
+            {
+                playerObject.AddForce(Vector2.up * jumpForce);
+                anim.SetBool("IsOnGround", false);
+            }
+            Animator anim;
 
-                {
-                playerObject.AddForce(Vector2.up*jumpForce);
-                }
+            void Start()
+            {
+                anim = GetComponent<Animator>();
+            }
 
+            void Update()
+            {
+                anim.SetFloat("speed", Mathf.Abs(movementValueX));
+                anim.SetBool("IsOnGround", false);
             }
         }
-
     }
+
+}
